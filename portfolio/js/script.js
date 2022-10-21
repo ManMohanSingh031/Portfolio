4620922e7f9cdd3614e50352f5346380da36a4ba
Man Mohan Singh <98742502+ManMohanSingh031@users.noreply.github.com>
2022-10-21 22:51:15 +0530

    Done

---
+++ portfolio/js/script.js
@@ -0,0 +1,13 @@
+
+var typed = new Typed(".typing",{
+  strings: [
+    "",
+    " Web Designer ",
+    " web Developer ",
+    " Graphic Designer ",
+    " YouTuber ",
+  ],
+  typeSpeed: 100,
+  BackSpeed: 60,
+  loop: true
+});
